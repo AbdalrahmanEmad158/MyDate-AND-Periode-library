1875cceef07ed47dd04166316dfc0c4cc76f8608
Abdalrahman Emad <abdoemad1582004@gmail.com>
2024-07-15 11:55:24 -0700

    Add files via upload

---
+++ App Date.cpp
@@ -0,0 +1,23 @@
+
+#include<iostream>
+#include"clsDate.h"
+using namespace std;
+int main()
+{
+	clsDate Date1;
+	Date1.Print();
+	clsDate Date2("31/1/2022");
+	Date2.Print();
+	clsDate Date3(20, 12, 2024);
+	Date3.Print();
+	clsDate Date4(250, 2024);
+	Date4.Print();
+	Date1.IncreaseDateByOneMonth();
+	Date1.Print();
+	Date3.PrintYearCalendar();
+	cout << Date2.IsValid() << endl;
+	cout << "My Age InDays:" << clsDate::CalculateMyAgeInDays(clsDate(15, 8, 2004));
+	//You can try any method at your own..  
+	system("pause>0");
+	return 0;
+}
